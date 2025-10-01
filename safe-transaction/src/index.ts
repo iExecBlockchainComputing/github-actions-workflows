bcfbab4cbd80fdff39342fcde6437fc52e3c24d4
Le-Caignec <robinlecaignec@yahoo.fr>
2025-10-01 14:30:38 +0200

    refactor: update API Kit and Protocol Kit initialization to use default exports

--- safe-transaction/src/index.ts
+++ safe-transaction/src/index.ts
@@ -35,13 +35,13 @@ async function run() {
     core.info(`🔑 Proposer Address: ${wallet.address}`);
 
     // Initialize API Kit
-    const apiKit = new SafeApiKit({
+    const apiKit = new SafeApiKit.default({
       chainId: 42161n, // Arbitrum
       apiKey: safeApiKey,
     });
 
     // Initialize Protocol Kit
-    const protocolKit = await Safe.create({
+    const protocolKit = await Safe.default.init({
       provider: rpcUrl,
       signer: proposerPrivateKey,
       safeAddress: safeAddress,
